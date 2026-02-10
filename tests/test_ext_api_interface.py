35805480c62edee392f4aade9e9cdfbd66c611e4
bdbvse <bdbvse@g.rit.edu>
2026-02-10 15:44:27 -0500

    Fix/improve sample tests.

--- tests/test_ext_api_interface.py
+++ tests/test_ext_api_interface.py
@@ -1,7 +1,7 @@
 import os
 import unittest
 from library import ext_api_interface
-from unittest.mock import Mock
+from unittest.mock import Mock, MagicMock
 import requests
 import json
 
@@ -18,9 +18,42 @@ class TestExtApiInterface(unittest.TestCase):
             self.json_data = json.loads(f.read())
 
     def test_is_book_available_connection_error(self):
+        # setup test
         ext_api_interface.requests.get = Mock(side_effect=requests.ConnectionError)
         self.assertFalse(self.api.is_book_available('bogus book'))
+        ext_api_interface.requests.get.assert_called()
 
     def test_get_ebooks(self):
-        self.api.make_request = Mock(return_value=self.json_data)
-        self.assertEqual(self.api.get_ebooks(self.book), self.books_data)
+        # setup test
+        response_mock = MagicMock()
+        response_mock.json = Mock(return_value=self.json_data)
+        response_mock.raise_for_status = Mock(return_value=None)
+        ext_api_interface.requests.get = Mock(return_value=response_mock)
+        # run test
+        result = self.api.get_ebooks(self.book)
+        # validate test
+        self.assertEqual(self.books_data, result)
+        ext_api_interface.requests.get.assert_called()
+        response_mock.raise_for_status.assert_called()
+        response_mock.json.assert_called()
+
+    def test_get_ebooks_2(self):
+        # setup test
+        response_mock = self.__make_mock_response(self.json_data)
+        ext_api_interface.requests.get = Mock(return_value=response_mock)
+        # run test
+        result = self.api.get_ebooks(self.book)
+        # validate test
+        self.assertEqual(self.books_data, result)
+        ext_api_interface.requests.get.assert_called()
+        self.__assert_mock_called(response_mock)
+
+    def __make_mock_response(self, json_data) -> MagicMock:
+        response_mock = MagicMock()
+        response_mock.json = Mock(return_value=json_data)
+        response_mock.raise_for_status = Mock(return_value=None)
+        return response_mock
+
+    def __assert_mock_called(self, response_mock):
+        response_mock.raise_for_status.assert_called()
+        response_mock.json.assert_called()
--- tests/test_library_db_interface.py
+++ tests/test_library_db_interface.py
@@ -8,30 +8,37 @@ class TestLibraryDBInterface(unittest.TestCase):
 
     def setUp(self):
         self.db_interface = library_db_interface.Library_DB()
+        self.patron = Patron('Fred', 'Cartz', 60, '47')
 
     def tearDown(self):
         self.db_interface.close_db()
+        self.db_interface = None
+        self.patron = None
 
     def test_insert_patron_not_in_db(self):
-        patron_mock = Mock()
+        # setup test
         self.db_interface.retrieve_patron = Mock(return_value=None)
-        data = {'fname': 'name', 'lname': 'name', 'age': 'age', 'memberID': 'memberID',
-                'borrowed_books': []}
-        self.db_interface.convert_patron_to_db_format = Mock(return_value=data)
-        self.db_interface.db.insert = Mock(side_effect=lambda x: 10 if x==data else 0)
-        self.assertEqual(self.db_interface.insert_patron(patron_mock), 10)
+        data = self.db_interface.convert_patron_to_db_format(self.patron)
+        self.db_interface.db.insert = Mock(side_effect = lambda x: 10 if x==data else 0)
+        # run test
+        result = self.db_interface.insert_patron(self.patron)
+        # validate test
+        self.assertEqual(10, result)
 
     def test_update_patron(self):
-        data = {'fname': 'name', 'lname': 'name', 'age': 'age', 'memberID': 'memberID',
-                'borrowed_books': []}
-        self.db_interface.convert_patron_to_db_format = Mock(return_value=data)
-        db_update_mock = Mock()
+        # setup test
+        db_update_mock = Mock(return_value=[1])
         self.db_interface.db.update = db_update_mock
-        self.db_interface.update_patron(Mock())
+        # run test
+        is_successful = self.db_interface.update_patron(self.patron)
+        # validate test
+        self.assertTrue(is_successful)
         db_update_mock.assert_called()
 
     def test_convert_patron_to_db_format(self):
-        patron = Patron('Fred', 'Cartz', 3, '4')
-        self.assertEqual(self.db_interface.convert_patron_to_db_format(patron),
-                         {'fname': 'Fred', 'lname': 'Cartz', 'age': 3, 'memberID': '4',
-                          'borrowed_books': []})
+        # setup test: N/A
+        # run test
+        result = self.db_interface.convert_patron_to_db_format(self.patron)
+        # validate test
+        self.assertEqual({'fname': 'Fred', 'lname': 'Cartz', 'age': 60, 'memberID': '47', 'borrowed_books': []},
+                         result)
